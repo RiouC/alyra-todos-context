dd0c95fa5832b82e75586e3c77bf64acaf674f49
RiouC <riou.christophe@protonmail.com>
2021-05-17 15:27:54 +0200

    [DarkMode] add darkmode context

---
+++ src/context/ColorModeContext.js
@@ -0,0 +1,11 @@
+import { createContext, useContext } from "react";
+
+export const DarkModeContext = createContext();
+
+export const useDarkMode = () => {
+    const context = useContext(DarkModeContext);
+    if (context === undefined) {
+        throw new Error("You should use useColormode only within the ColorModeContext.Provider");
+    }
+    return context;
+};
